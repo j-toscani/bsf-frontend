6b5e0dd565ecd4f175d93aa30df163b51e2e24aa
WSL: Julian Toscani <julian_toscani@gmail.com>
2021-05-06 23:06:12 +0200

    add custom input

---
+++ components/CustomTextInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="inpue__container">
+    <label :for="$attrs.id" class="input__label">
+      {{ label }}
+    </label>
+    <input
+      v-bind="$attrs"
+      class="input__input"
+      :vale="value"
+      @input="$emit('input', value)"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "CustomTextInput",
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+  data(): {
+    value: string;
+  } {
+    return {
+      value: "",
+    };
+  },
+});
+</script>
+
+<style>
+</style>
