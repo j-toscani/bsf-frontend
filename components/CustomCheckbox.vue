0564c94bb56a09303511bd135878c46f215c17b8
WSL: Julian Toscani <julian_toscani@gmail.com>
2021-05-08 14:48:31 +0200

    finetune custom checkbox

--- components/CustomCheckbox.vue
+++ components/CustomCheckbox.vue
@@ -2,13 +2,14 @@
 <template>
   <div class="checkbox__wrapper">
     <input
-      class="checkbox__input"
       style="display: none"
+      type="checkbox"
       v-bind="$attrs"
       :value="value"
     />
-    <label class="checkbox__label" :for="$attrs.id"> {{ label }} </label>
-    <div class="checkbox__dspbox"></div>
+    <label class="checkbox__label" :for="$attrs.id">
+      <span> {{ label }} </span>
+    </label>
   </div>
 </template>
 
@@ -30,3 +31,27 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style scoped>
+.checkbox__label::after {
+  content: "\.";
+  line-height: 1;
+  border-radius: 5px;
+
+  display: inline-flex;
+  align-content: center;
+  justify-content: center;
+  background: rgb(250, 250, 250);
+  color: rgb(250, 250, 250);
+
+  border: 1px solid var(--color-100, #ccc);
+  height: 1.25em;
+  width: 1.25em;
+}
+
+input[type="checkbox"]:checked + .checkbox__label::after,
+input[type="radio"]:checked + .checkbox__label::after {
+  content: "\002714";
+  color: inherit;
+}
+</style>
