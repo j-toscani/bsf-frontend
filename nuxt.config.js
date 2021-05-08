de0022dbf9faff09d40c5eace735366ac88f7be9
WSL: Julian Toscani <julian_toscani@gmail.com>
2021-05-08 13:51:29 +0200

    move page css to global.css

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "icon", type: "text/css", href: "/global.css" }]
   },
 
   server: {
