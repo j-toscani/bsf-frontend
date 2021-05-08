8d3e73be3803b52fecb20d012396f1f72424413a
WSL: Julian Toscani <julian_toscani@gmail.com>
2021-05-08 14:49:21 +0200

    add custom inputs to components page

--- pages/components.vue
+++ pages/components.vue
@@ -1,9 +1,30 @@
 <template>
-  <div class="container components-container"></div>
+  <div class="container components-container">
+    <CustomCheckbox label="Ceckbox 1" id="firstbox" :value="true" />
+    <CustomCheckbox label="Ceckbox 2" id="secondbox" :value="false" />
+    <CustomSelect label="Players:" :options="players" />
+    <CustomInput type="password" label="Pasword" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-export default Vue.extend({});
+import CustomCheckbox from "@/components/CustomCheckbox.vue";
+import CustomSelect from "@/components/CustomSelect.vue";
+import CustomInput from "@/components/CustomInput.vue";
+
+export default Vue.extend({
+  name: "components",
+  components: {
+    CustomCheckbox,
+    CustomSelect,
+    CustomInput,
+  },
+  data() {
+    return {
+      players: ["Mike", "Flo", "Julian", "Armin"],
+    };
+  },
+});
 </script>
