e655b0354fad3e55eb87481f4ca5d590b8f7025c
WSL: Julian Toscani <julian_toscani@gmail.com>
2021-05-08 14:02:58 +0200

    add custom select

--- components/CustomSelect.vue
+++ components/CustomSelect.vue
@@ -1,84 +1,58 @@
 
 <template>
   <div class="select__wrapper">
-    <div class="select__display">
-      <slot></slot>
-    </div>
-    <ul class="select__list">
-      <li
-        v-for="(option, index) in dspOptions"
-        class="select__list-item"
-        :key="option.id + '-' + index"
+    <label class="select__label" :for="$attrs.id">{{ label }}</label>
+    <select class="select__input" v-bind="$attrs" v-model="picks">
+      <option
+        class="select__option"
+        v-for="(option, index) in options"
+        :key="index"
+        :value="option"
       >
-        <input
-          :type="type"
-          style="display: none"
-          :value="option"
-          :name="option.dspName"
-          :id="option.id"
-          v-model="picks"
-        />
-        <label :for="option.id"> {{ option.dspValue }} </label>
-        <div class="select__list-item-dspbox"></div>
-      </li>
-    </ul>
+        {{ getDspValue(option) }}
+      </option>
+    </select>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue, { PropOptions } from "vue";
 export default Vue.extend({
+  inheritAttrs: false,
+  name: "CustomSelect",
   props: {
-    getDspValue: {
-      type: Function,
-      default: (value: any) => value,
-    },
-    getOptionId: {
-      type: Function,
-      default: (value: any, index: number) =>
-        value?.id ? value?.id : value + "-" + index,
-    },
-    chosen: {
+    value: {
       type: Array,
       default: () => [],
     },
-    multi: {
-      type: Boolean,
-      default: false,
+    getDspValue: {
+      type: Function,
+      default: (option: any) => {
+        option;
+      },
     },
     options: {
       type: Array,
-      required: true,
-    },
+      default: () => [],
+    } as PropOptions<any[]>,
   },
   data() {
     return {
-      picks: this.chosen,
+      picks: this.value,
     };
   },
   watch: {
-    chosen: {
+    value: {
       deep: true,
       handler() {
-        this.picks = this.chosen;
+        this.picks = this.value;
       },
     },
-  },
-  methods: {
-    allElementsAreSameType(type: string) {
-      return this.options.every((option) => typeof option === type);
-    },
-  },
-  computed: {
-    type(): "radio" | "checkbox" {
-      return this.multi ? "radio" : "checkbox";
-    },
-    dspOptions() {
-      return this.options.map((option, index) => ({
-        id: this.getOptionId(option, index),
-        dspValue: this.getDspValue(option),
-        value: option,
-      }));
+    picks: {
+      deep: true,
+      handler() {
+        this.$emit("input", this.picks);
+      },
     },
   },
 });
