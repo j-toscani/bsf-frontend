324d92f90d5cf7a1483778ee48c4c133d78dd409
WSL: Julian Toscani <julian_toscani@gmail.com>
2021-05-09 21:01:22 +0200

    add tournamentlist component

---
+++ components/TournamentList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2><slot /></h2>
+    <ul class="tournaments__list">
+      <li
+        class="tournaments__list-item item"
+        v-for="(tournament, index) in tournaments"
+        :key="index"
+      >
+        <nuxt-link :to="baseUrl + tournament.id">
+          {{ tournament.name }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "TournamentList",
+  props: {
+    tournaments: {
+      type: Array,
+      default: () => [],
+    },
+    baseUrl: {
+      type: String,
+      default: "/",
+      validator: (baseUrl) => baseUrl.startsWith("/"),
+    },
+  },
+});
+</script>
+
+<style scoped>
+</style>
