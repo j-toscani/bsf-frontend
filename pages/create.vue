9772d2e5022aa798c1a7bb492cf4ab4ea4bab4e2
WSL: Julian Toscani <julian_toscani@gmail.com>
2021-05-09 22:34:38 +0200

    remove nav

--- pages/create.vue
+++ pages/create.vue
@@ -1,14 +1,8 @@
 <template>
-  <main>
+  <div>
     <h1>Create your Tournament</h1>
-    <div>
-      <nav>
-        <nuxt-link to="/create/"> Contestants </nuxt-link>
-        <nuxt-link to="/create/teams"> Teams </nuxt-link>
-      </nav>
-    </div>
     <nuxt-child />
-  </main>
+  </div>
 </template>
 
 <script lang="ts">
