9f0f12830236a41797fb0bc5358fe51491320407
WSL: Julian Toscani <julian_toscani@gmail.com>
2021-05-09 18:29:40 +0200

    add layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="layout__app-wrapper">
+    <header></header>
+    <Nuxt class="layout__container" />
+    <footer></footer>
   </div>
 </template>
+
+<style scoped>
+footer {
+  height: 10rem;
+  width: 100%;
+}
+
+header {
+  height: 7.5rem;
+  width: 100%;
+}
+
+.layout__app-wrapper {
+  min-height: 100vh;
+  width: 100%;
+}
+
+.layout__container {
+  min-height: calc(100vh - 17.5rem);
+}
+</style>
