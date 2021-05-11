1e9c64629798e5fa10ae1165a4b312fb555e7777
WSL: Julian Toscani <julian_toscani@gmail.com>
2021-05-11 23:28:13 +0200

    create and extend types and constants for testing

---
+++ helper/constants.ts
@@ -0,0 +1,14 @@
+export const TEAM_OPTIONS = [
+  { name: "1v1", value: 1 },
+  { name: "2v2", value: 2 },
+  { name: "3v3", value: 3 },
+  { name: "4v4", value: 4 }
+] as const;
+
+export const CONTESTANTS = [
+  "Mike",
+  "Flo",
+  "Armin",
+  "Julian P.",
+  "Julian T."
+] as const;
--- types/types.ts
+++ types/types.ts
@@ -1,1 +1,12 @@
-export type Contestant = string;
+import { TEAM_OPTIONS, CONTESTANTS } from "@/helper/constants";
+
+export type Contestant = typeof CONTESTANTS[number];
+export interface Team {
+  name: string;
+  contestants: Contestant[];
+}
+
+export type TeamSize = typeof TEAM_OPTIONS[number];
+
+const values = TEAM_OPTIONS.map(option => option.value);
+export type TeamSizeValue = typeof values[number];
