355cede9625bc4ac8e05db577442ccf5ca0bff7d
WSL: Julian Toscani <julian_toscani@gmail.com>
2021-05-11 23:29:46 +0200

    add teams form

--- pages/create/teams.vue
+++ pages/create/teams.vue
@@ -1,12 +1,67 @@
 <template>
-  <h2>This is the Teams Page.</h2>
+  <section>
+    <h2>This is the Teams Page.</h2>
+    <CustomSelect
+      label="Team Size"
+      :options="teamOptions"
+      v-model="teamSize"
+      :getDspValue="getTeamTypeName"
+    />
+    <ol>
+      <li v-for="(team, tIndex) in teams" :key="'team' + tIndex">
+        <h4>{{ team.name }}</h4>
+        <ul class="has-list-style">
+          <li
+            v-for="(slot, cIndex) in slotsInTeams"
+            :key="'team' + tIndex + 'contestant' + cIndex"
+          >
+            {{ team.contestants[slot] ? team.contestants[slot] : "---" }}
+          </li>
+        </ul>
+      </li>
+    </ol>
+  </section>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { mapGetters } from "vuex";
+import { Team } from "@/types/types";
+
+import CustomSelect from "~/components/CustomSelect.vue";
 
 export default Vue.extend({
+  components: { CustomSelect },
   name: "Teams",
+  asyncData({ store }) {
+    store.dispatch("create/setTeamSize", 1);
+    return {
+      teamSize: store.state.create.teamOptions[0],
+      teamOptions: store.state.create.teamOptions,
+    };
+  },
+  watch: {
+    teamSize(teamSize) {
+      this.$store.dispatch("create/setTeamSize", teamSize.value);
+    },
+  },
+  computed: {
+    ...mapGetters({
+      maxNumberOfTeams: "create/maxNumberOfTeams",
+    }),
+    teams(): Team[] {
+      return this.$store.state.create.teams;
+    },
+    slotsInTeams(): number[] {
+      const teamSize = this.$store.state.create.teamSize;
+      return Array.from(new Array(teamSize), (_e, i) => i);
+    },
+  },
+  methods: {
+    getTeamTypeName(data: { name: string; value: number }) {
+      return data.name;
+    },
+  },
 });
 </script>
 
