612783dc15a2d5ce6715312728d66f9156fc2128
WSL: Julian Toscani <julian_toscani@gmx.de>
2021-05-19 18:26:51 +0200

    add derived types to createBaseRepository

--- helper/createBaseRepository.ts
+++ helper/createBaseRepository.ts
@@ -1,22 +1,27 @@
 import { Context } from "@nuxt/types";
-
+import { Ressource, RessourceType } from "@/types/derivedTypes";
 // Based on https://blog.lichter.io/posts/nuxt-api-call-organization-and-decoupling/
-const createBaseRepository = (ctx: Context) => (ressource: string) => ({
-  getAll() {
-    return ctx.$axios.$get(`/${ressource}`);
-  },
-  getOne(query: string) {
-    return ctx.$axios.$get(`/${ressource}/${query}`);
-  },
-  create(payload: any) {
-    return ctx.$axios.$post(`/${ressource}`, payload);
-  },
-  update(payload: any) {
-    return ctx.$axios.$put(`/${ressource}`, payload);
-  },
-  delete(payload: any) {
-    return ctx.$axios.$delete(`/${ressource}`, payload);
-  }
-});
+
+const createBaseRepository = <T extends Ressource>(ctx: Context) => (
+  ressource: T
+) => {
+  return {
+    getAll(): Promise<RessourceType<T>[]> {
+      return ctx.$axios.$get(`/${ressource}`);
+    },
+    getOne(query: string): Promise<RessourceType<T>> {
+      return ctx.$axios.$get(`/${ressource}/${query}`);
+    },
+    create(payload: RessourceType<T>): Promise<RessourceType<T>> {
+      return ctx.$axios.$post(`/${ressource}`, payload);
+    },
+    update(id: string, payload: any): Promise<RessourceType<T>> {
+      return ctx.$axios.$put(`/${ressource}/${id}`, payload);
+    },
+    delete(id: string): Promise<RessourceType<T>> {
+      return ctx.$axios.$delete(`/${ressource}/${id}`);
+    }
+  };
+};
 
 export default createBaseRepository;
