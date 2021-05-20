aaa3984671cf1675f3b65821ed113e26be429420
WSL: Julian Toscani <julian_toscani@gmx.de>
2021-05-20 13:25:31 +0200

    WIP create factory to create matchubs for tournaments

--- helper/createMatchups.ts
+++ helper/createMatchups.ts
@@ -6,18 +6,34 @@ import createGetPidForTournament from "./createGetPidForTournament";
 import createTournamentPerformance from "./createTournamentPerformance";
 import createGame from "@/helper/createGame";
 
-type PerformanceCreator = ReturnType<typeof createTournamentPerformance>;
-
-function createMatchupCreator(api: Api) {
-  return (tournament: ApiTournament) => {
+export default function createTournamentMatchupCreator(api: Api) {
+  return function createMatchupCreator(tournament: ApiTournament) {
     const getPid = createGetPidForTournament(tournament);
     const getGid = createGetGidForTournament(tournament);
     const createPlayerPerformance = createTournamentPerformance(tournament);
 
-    return async function createMatchup(teamA: Team, teamB: Team) {
+    return async function createMatchup(teams: [Team, Team], matchNr: number) {
+      const [teamA, teamB] = teams;
       const g_id = getGid(teamA.name, teamB.name);
-      const game = createGame(g_id, [teamA, teamB]);
-      const result = await api.games.create(game);
+      const game = createGame({ g_id, tournamentId: tournament.id }, [
+        teamA,
+        teamB
+      ]);
+      const createdGame = await api.games.create(game);
+      const players = [...teamA.players, ...teamB.players];
+
+      const performanceRequests = players.map(player => {
+        const p_id = getPid(player.gamertag, matchNr);
+        const performance = createPlayerPerformance(player, {
+          p_id,
+          game_id: createdGame.id!
+        });
+        return api.performances.create(performance);
+      });
+
+      const performances = await Promise.all(performanceRequests);
+      const performanceIds = performances.map(performance => performance.id!);
+      api.games.update(createdGame.id!, { performances: performanceIds });
     };
   };
 }
