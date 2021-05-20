c9f2f2308721e9acb813cf2c6723a7835c590554
WSL: Julian Toscani <julian_toscani@gmx.de>
2021-05-20 23:32:28 +0200

    create page to display tournaments

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,13 +1,67 @@
 <template>
   <div>
-    {{ $route.params.id }}
+    <h1>{{ tournament.name }}</h1>
+    <h2>Contestants:</h2>
+    <ul class="has-list-style">
+      <li v-for="(contenstant, index) in contestants" :key="index">
+        {{ contenstant.gamertag }}
+      </li>
+    </ul>
+    <h2>Matchups:</h2>
+    <ul>
+      <li v-for="(game, index) in games" :key="index">
+        <div>
+          <span>
+            <span> {{ game.name_team_a }} </span> :
+            <span> ({{ game.teams.team_a | getTeamMembers }}) </span>
+          </span>
+          <strong>vs</strong>
+          <span>
+            <span> {{ game.name_team_b }} </span> :
+            <span>({{ game.teams.team_b | getTeamMembers }})</span>
+          </span>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { ApiGame, ApiPlayer, ApiTournament } from "~/types/types";
 export default Vue.extend({
   name: "Tournament",
+  async asyncData({ $api, route }) {
+    const tournamentId = route.params.id;
+
+    const tournament = await $api.tournaments.getOne(tournamentId);
+    return {
+      tournament,
+    };
+  },
+  data(): {
+    tournament: ApiTournament | null;
+  } {
+    return {
+      tournament: null,
+    };
+  },
+  filters: {
+    getTeamMembers(value: ApiPlayer[]) {
+      const tagsOfTeamMembers = value.map((player) => player.gamertag);
+      return tagsOfTeamMembers.join(", ");
+    },
+  },
+  computed: {
+    contestants(): ApiPlayer[] {
+      return this.tournament
+        ? (this.tournament.contestants as ApiPlayer[])
+        : [];
+    },
+    games(): ApiGame[] {
+      return this.tournament ? (this.tournament.games as ApiGame[]) : [];
+    },
+  },
 });
 </script>
 
