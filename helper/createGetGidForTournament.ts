564a9bb67511a2fe8296a04cb118070ab65c28ba
WSL: Julian Toscani <julian_toscani@gmx.de>
2021-05-20 20:21:22 +0200

    fix gid creation

--- helper/createGetGidForTournament.ts
+++ helper/createGetGidForTournament.ts
@@ -4,7 +4,9 @@ export default function createGetGidForTournament(tournament: ApiTournament) {
   return (teamNameA: string, teamNameB: string) => {
     const formattedNames = [teamNameA, teamNameB].map(trimAndFormat);
     const [formattedNameA, formattedNameB] = formattedNames;
-    return `${tournament.name}__${formattedNameA}_vs_${formattedNameB}`;
+    return `${tournament.name
+      .split(" ")
+      .join("_")}__${formattedNameA}_vs_${formattedNameB}`;
   };
 }
 
