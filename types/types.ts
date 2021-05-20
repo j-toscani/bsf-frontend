02f2e4ce75ebcc83ee6f64685239c6941fc8fce9
WSL: Julian Toscani <julian_toscani@gmx.de>
2021-05-20 10:08:30 +0200

    add performance type

--- types/types.ts
+++ types/types.ts
@@ -32,7 +32,37 @@ export interface Tournament {
   id: string;
 }
 
-export interface Performance {}
+export interface Performance {
+  _id?: string;
+  points: {
+    _id?: string;
+    goals: number;
+    points: number;
+    assists: number;
+    saves: number;
+    shots: number;
+    __v?: number;
+    id?: string;
+  };
+  createdAt?: Date;
+  updatedAt?: Date;
+  __v?: 1;
+  player: {
+    discord: boolean;
+    telegram: boolean;
+    mail: boolean;
+    participated_in: [string];
+    _id?: string;
+    gamertag: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    __v?: number;
+    id?: string;
+  };
+  p_id: string;
+  game: string;
+  id?: string;
+}
 
 export interface Team {
   name: string;
