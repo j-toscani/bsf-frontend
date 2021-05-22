e23a39b5f31b60d352bf9bf9d4c7300490da267d
WSL: Julian Toscani <julian_toscani@gmx.de>
2021-05-23 00:25:27 +0200

    add all forms to popup

---
+++ components/CustomPopupLogin.vue
@@ -0,0 +1,34 @@
+<template>
+  <CustomPopup @close="emitClose">
+    <FormLogin />
+    <FormRecover />
+    <FormRegister />
+  </CustomPopup>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import CustomPopup from "@/components/CustomPopup.vue";
+import FormLogin from "@/components/FormLogin.vue";
+import FormRecover from "@/components/FormRecover.vue";
+import FormRegister from "@/components/FormRegister.vue";
+
+export default Vue.extend({
+  components: {
+    CustomPopup,
+
+    FormLogin,
+    FormRecover,
+    FormRegister,
+  },
+  methods: {
+    emitClose() {
+      this.$emit("close");
+    },
+  },
+});
+</script>
+
+<style scoped>
+</style>
