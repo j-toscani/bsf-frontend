d78ca8d23ca9148fa36da6a5a6915e22cb2d73dc
WSL: Julian Toscani <julian_toscani@gmx.de>
2021-05-23 00:22:49 +0200

    add login form

---
+++ components/FormLogin.vue
@@ -0,0 +1,34 @@
+<template>
+  <form id="login" @submit.prevent="handleSubmit">
+    <CustomInput type="email" label="Email" v-model="email" />
+    <CustomInput type="password" label="Password" v-model="password" />
+    <CustomButton form="login" type="submit" level="tertiary">
+      Login!
+    </CustomButton>
+  </form>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import CustomInput from "./CustomInput.vue";
+import CustomButton from "@/components/CustomButton.vue";
+
+export default Vue.extend({
+  components: { CustomInput, CustomButton },
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const payload = { email: this.email, password: this.password };
+      console.log(payload);
+    },
+  },
+});
+</script>
+
+<style scoped>
+</style>
