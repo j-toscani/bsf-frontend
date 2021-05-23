dc2f17f54246607b7ae792660c7fafece471a27f
WSL: Julian Toscani <julian_toscani@gmx.de>
2021-05-23 18:33:31 +0200

    check tournament for Ownership

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>{{ tournament.name }}</h1>
+    <h1>{{ tournamentName }}</h1>
 
     <h2>Matchups:</h2>
     <ul>
@@ -14,6 +14,7 @@
             v-for="(performance, index) of performances"
             :key="index"
             :performance="performance"
+            :isOwn="isOwn"
           />
         </template>
       </PerformanceTeamsWrapper>
@@ -39,22 +40,39 @@ export default Vue.extend({
     PerformanceTeamsWrapper,
     PerformanceRocketLeageForm,
   },
-  async asyncData({ $api, route }) {
+  async asyncData({ $api, route, $auth }) {
     const tournamentId = route.params.id;
 
-    const tournament = await $api.tournaments.getOne(tournamentId);
+    let tournament = null;
+    let isOwn = false;
+    try {
+      tournament = (await $api.tournaments.getOne(
+        tournamentId
+      )) as ApiTournament<ApiPerformanceComponentName>;
+
+      const requestedByHost = $auth.user
+        ? tournament.host === $auth.user.id
+        : false;
+
+      isOwn = requestedByHost;
+    } catch (error) {
+      console.error(error);
+    }
     return {
       tournament,
+      isOwn,
     };
   },
   data(): {
-    tournament: ApiTournament<ApiPerformanceComponentName> | null;
+    tournament?: ApiTournament<ApiPerformanceComponentName> | null;
+    isOwn?: boolean;
   } {
-    return {
-      tournament: null,
-    };
+    return {};
   },
   computed: {
+    tournamentName(): string {
+      return this.tournament ? this.tournament.name : "Not Found";
+    },
     games(): ApiGame<ApiPerformanceComponentName>[] {
       return this.tournament
         ? (this.tournament.games as ApiGame<ApiPerformanceComponentName>[])
